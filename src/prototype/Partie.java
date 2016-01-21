2318cc472f988e369ec39054d8b6229f53b544f4
Lumen <credeville.louis.maxime@gmail.com>
2016-01-21 20:25:49 +0100

    retif

--- src/prototype/Partie.java
+++ src/prototype/Partie.java
@@ -1,11 +1,11 @@
 package prototype;
 
 public class Partie {
-    public static void main(String argv[]) {
+    public Partie() {
 	Grille grille = new Grille(3);
 	AbstractJoueur js[] = new AbstractJoueur[2];
-	// js[0] = new Joueur(1);
-	js[0] = new JoueurSimplet(1);
+	js[0] = new Joueur(1);
+	// js[0] = new JoueurSimplet(1);
 	js[1] = new JoueurSimplet(2);
 	int j = 0;
 	while (!grille.isPlein()) {
--- src/prototype/jeu.java
+++ src/prototype/jeu.java
@@ -2,9 +2,10 @@ package prototype;
 
 public class jeu {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-/*Test VR*/
-	}
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	new Partie();
+	/* Test VR */
+    }
 
 }

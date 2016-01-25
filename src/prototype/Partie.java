2549b356df3059075453f3f6ea8e5cc74ab2fcf9
Lumen <credeville.louis.maxime@gmail.com>
2016-01-25 10:41:14 +0100

    sup test

--- src/prototype/Partie.java
+++ src/prototype/Partie.java
@@ -2,11 +2,11 @@ package prototype;
 
 public class Partie {
     public Partie() {
-	Grille grille = new Grille(3);
+	Grille grille = new Grille(5);
 	AbstractJoueur js[] = new AbstractJoueur[2];
 	js[0] = new Joueur(1);
-	// js[0] = new JoueurSimplet(1);
-	js[1] = new JoueurSimplet(2);
+	 js[0] = new JoueurSimplet(1);
+	//js[1] = new JoueurSimplet(2);
 	int j = 0;
 	while (!grille.isPlein()) {
 	    System.out.println(grille);
--- src/prototype/testpe.java
+++
@@ -1,8 +0,0 @@
-package prototype;
-
-public class testpe {
-	public static void main(String[]args){
-		System.out.println("HW!");
-		System.out.println("HW!");
-	}
-}

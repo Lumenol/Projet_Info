e69815f97268c2465089d851757902090bea7f2c
Lumen <credeville.louis.maxime@gmail.com>
2016-01-25 12:13:06 +0100

    nombre carré complete non testé

--- src/prototype/Grille.java
+++ src/prototype/Grille.java
@@ -16,6 +16,51 @@ public class Grille {
 	}
     }
 
+    public int carreComplete(int x, int y) {
+	int c = 0;
+	int[][] p = { { -1, -1 }, { 0, -2 }, { 1, -1 } };
+	if (y % 2 == 0) {
+	    boolean b = true;
+	    for (int i = 0; i < p.length && b; i++) {
+		int x_ = x + p[i][0];
+		int y_ = y + p[i][1];
+		if (x_ >= 0 && x_ < grille.length && y_ >= 0 && y_ < grille.length) {
+		    b = grille[y_][x_] != 0;
+		}
+	    }
+	    c += b ? 1 : 0;
+	    b = true;
+	    for (int i = 0; i < p.length && b; i++) {
+		int x_ = x - p[i][0];
+		int y_ = y - p[i][1];
+		if (x_ >= 0 && x_ < grille.length && y_ >= 0 && y_ < grille.length) {
+		    b = grille[y_][x_] != 0;
+		}
+	    }
+	    c += b ? 1 : 0;
+	} else {
+	    boolean b = true;
+	    for (int i = 0; i < p.length && b; i++) {
+		int x_ = x + p[i][1];
+		int y_ = y + p[i][0];
+		if (x_ >= 0 && x_ < grille.length && y_ >= 0 && y_ < grille.length) {
+		    b = grille[y_][x_] != 0;
+		}
+	    }
+	    c += b ? 1 : 0;
+	    b = true;
+	    for (int i = 0; i < p.length && b; i++) {
+		int x_ = x - p[i][1];
+		int y_ = y - p[i][0];
+		if (x_ >= 0 && x_ < grille.length && y_ >= 0 && y_ < grille.length) {
+		    b = grille[y_][x_] != 0;
+		}
+	    }
+	    c += b ? 1 : 0;
+	}
+	return c;
+    }
+
     public boolean isPlein() {
 	for (int i = 0; i < grille.length; i++) {
 	    for (int j = 0; j < grille.length; j++) {

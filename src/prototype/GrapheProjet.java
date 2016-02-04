bc10ded4f0e64650de425aa8a56a71dbdedc72f6
Lumen <credeville.louis.maxime@gmail.com>
2016-02-04 18:43:39 +0100

    Arbre .dot et .png

---
+++ src/prototype/GrapheProjet.java
@@ -0,0 +1,23 @@
+package prototype;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GrapheProjet {
+
+    public static void main(String[] args) {
+
+	Arbre a = new Arbre(1);
+
+	try {
+	    FileWriter fw = new FileWriter("autre/out.dot");
+	    fw.write(a.toString());
+	    System.out.println("OK");
+	    fw.close();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
+
+}
--- src/prototype/Sommet.java
+++ src/prototype/Sommet.java
@@ -1,11 +1,49 @@
 package prototype;
 
-public interface Sommet {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
-    int nbSuivants();
+public class Sommet {
+    private static int numero = 0;
+    private List<Sommet> voisin;
+    private Grille grille;
+    private int[] tableaeu;
+    private int num;
 
-    int poids();
+    public Sommet(int[] grille) {
+	voisin = new LinkedList<Sommet>();
+	tableaeu = grille;
+	this.grille = new Grille(Grille.dimmentionGrille(grille.length), grille);
+	num = numero;
+	numero++;
+    }
 
-    int points();
+    public boolean add(Sommet arg0) {
+	return voisin.add(arg0);
+    }
+
+    public Grille getGrille() {
+	return grille;
+    }
+
+    public int[] getTableaeu() {
+	return tableaeu;
+    }
+
+    public Iterator<Sommet> iterator() {
+	return voisin.iterator();
+    }
+
+    public String toString() {
+	StringBuffer sb = new StringBuffer(num + " [label=\"" + grille.toString(true) + "\"]\n");
+
+	for (Iterator iterator = voisin.iterator(); iterator.hasNext();) {
+	    Sommet sommet = (Sommet) iterator.next();
+	    sb.append(num + " -> " + sommet.num + "\n");
+	    sb.append(sommet);
+	}
+	return sb.toString();
+    }
 
 }

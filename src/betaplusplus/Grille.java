9275ec14b8bb4d0d5991e74086008d673e4968be
Lumen <credeville.louis.maxime@gmail.com>
2016-04-28 22:39:21 +0200

    NombreCarre

--- src/betaplusplus/Grille.java
+++ src/betaplusplus/Grille.java
@@ -1,45 +1,60 @@
 package betaplusplus;
+
 /**
- * 
+ *
  * @author Julie
- * 
+ *
  */
-public class Grille implements Etat{
-final static int BLOQUE=1;
-final static int JOUER=2;
-final static int VIDE=0;
-private int [][] grille;
-public boolean isPlein(){
-	return false;
-}
+public class Grille implements Etat {
+    final static int BLOQUE = 1;
+    final static int JOUER = 2;
+    final static int VIDE = 0;
+    private int[][] grille;
 
-public int placer(int x,int y){
-	return 0;
-}
-
-public int get (int x, int y){
-	try {
-		return grille[x][y];
-	}
-	catch (ArrayIndexOutOfBoundsException e) {
-		return BLOQUE;
-	}
-}
-
-public void RempliCarres() {
-	
-}
-
-private boolean CarreComplet (int x, int y){
-	return true;
-}
-
-@Override
-public boolean equals(Object obj) {
+    @Override
+    public boolean equals(Object obj) {
 	// TODO Auto-generated method stub
 	return super.equals(obj);
-}
+    }
 
+    public int get(int x, int y) {
+	try {
+	    return grille[x][y];
+	} catch (ArrayIndexOutOfBoundsException e) {
+	    return BLOQUE;
+	}
+    }
 
+    public int hauteur() {
+	return 0;
+
+    }
+
+    public boolean isPlein() {
+	return false;
+    }
+
+    @Override
+    public String label() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    public int largeur() {
+	return 0;
+
+    }
+
+    public int placer(int x, int y) {
+	return 0;
+    }
+
+    public void RempliCarres() {
+
+    }
+
+    private boolean CarreComplet(int x, int y) {
+	return true;
+    }
 
 }

13a2356e764b963d7669ae92c3ea982446f1e370
Lumen <credeville.louis.maxime@gmail.com>
2016-05-09 19:16:32 +0200

    Prevoyant

--- src/betaplusplus/Prevoyant.java
+++ src/betaplusplus/Prevoyant.java
@@ -1,9 +1,35 @@
 package betaplusplus;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Prevoyant extends AbstractSuccesseurs<Grille> {
 
-    public Prevoyant(Fonction<Grille, Iterable<Grille>> fonction) {
-	super(fonction);
+    public Prevoyant() {
+	super(new Fonction<Grille, Iterable<Grille>>() {
+
+	    private Simplet simplet = new Simplet();
+	    private NombreCarre nbC = new NombreCarre();
+
+	    @Override
+	    public Iterable<Grille> get(Grille x) {
+		LinkedList<Grille> retour = new LinkedList<>();
+		int min = Integer.MAX_VALUE;
+		for (Iterator<Grille> iterator = simplet.get(x).iterator(); iterator.hasNext();) {
+		    Grille y = iterator.next();
+		    int m = nbC.get(x) - nbC.get(y);
+		    if (m <= min) {
+
+			if (m < min) {
+			    min = m;
+			    retour.clear();
+			}
+			retour.add(y);
+		    }
+		}
+		return retour;
+	    }
+	});
 	// TODO Auto-generated constructor stub
     }
 

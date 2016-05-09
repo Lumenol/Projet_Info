a8d1dbd238f7bba79fb71c10c7e1f18f1c587488
Lumen <credeville.louis.maxime@gmail.com>
2016-05-09 19:32:02 +0200

    Idiot et correction Prevoyant

--- src/betaplusplus/Idiot.java
+++ src/betaplusplus/Idiot.java
@@ -3,9 +3,9 @@ package betaplusplus;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class Prevoyant extends AbstractSuccesseurs<Grille> {
+public class Idiot extends AbstractSuccesseurs<Grille> {
 
-    public Prevoyant() {
+    public Idiot() {
 	super(new Fonction<Grille, Iterable<Grille>>() {
 
 	    private Simplet simplet = new Simplet();
@@ -14,14 +14,14 @@ public class Prevoyant extends AbstractSuccesseurs<Grille> {
 	    @Override
 	    public Iterable<Grille> get(Grille x) {
 		LinkedList<Grille> retour = new LinkedList<>();
-		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 		for (Iterator<Grille> iterator = simplet.get(x).iterator(); iterator.hasNext();) {
 		    Grille y = iterator.next();
 		    int m = nbC.get(x) - nbC.get(y);
-		    if (m >= max) {
+		    if (m <= min) {
 
-			if (m > max) {
-			    max = m;
+			if (m < min) {
+			    min = m;
 			    retour.clear();
 			}
 			retour.add(y);

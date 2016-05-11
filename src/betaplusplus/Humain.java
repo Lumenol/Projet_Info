474e7d32963a1c056fa0bed17626f163f52ce2de
Bovie Pierre-Edouard <pe.xboxlive@hotmail.fr>
2016-05-11 21:50:28 +0200

    humain v0.1 semble fonctionner

--- src/betaplusplus/Humain.java
+++ src/betaplusplus/Humain.java
@@ -1,11 +1,33 @@
 package betaplusplus;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Humain implements Fonction<Grille, Iterable<Grille>> {
 
-    @Override
-    public Iterable<Grille> get(Grille x) {
-	// TODO Auto-generated method stub
-	return null;
-    }
+	public Humain(){
 
+
+	}
+	public Iterable<Grille> jouer(Grille grille){
+		HashSet<Grille> g = new HashSet<>();
+		Scanner sc = new Scanner(System.in);
+		int x = 0, y = 0;
+		do{
+			System.out.println("position largeur x: ");
+			x = sc.nextInt();
+			System.out.println("position hauteur y: ");
+			y = sc.nextInt();
+
+		}while (grille.get(x, y) != Grille.VIDE);
+		grille.placer(x, y);
+		grille.RempliCarres();
+		g.add(grille);
+		return g;
+	}
+	@Override
+	public Iterable<Grille> get(Grille x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

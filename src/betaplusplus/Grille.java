da4ca9c6e70af3a33edd02b727da1de4aa998aa7
RiverWilliams <julie.roche59@sfr.fr>
2016-05-11 21:35:17 +0200

    correction "contours"

--- src/betaplusplus/Grille.java
+++ src/betaplusplus/Grille.java
@@ -26,10 +26,10 @@ public class Grille implements Etat {
      * 
      * @param hauteur
      * @param largeur
-     * @param contoure 
+     * @param contours 
      */
-    public Grille(int hauteur, int largeur, boolean contoure) {
-	this(hauteur, largeur, contoure, null);
+    public Grille(int hauteur, int largeur, boolean contours) {
+	this(hauteur, largeur, contours, null);
     }
 
     /**Contruit la Grille suivant les parametres passés  

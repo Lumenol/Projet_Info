d52798e971f56459ab287e34ecb9868cffe3c163
Lumen <credeville.louis.maxime@gmail.com>
2016-05-11 22:17:26 +0200

    H++

--- src/betaplusplus/Humain.java
+++ src/betaplusplus/Humain.java
@@ -5,34 +5,26 @@ import java.util.Scanner;
 
 public class Humain implements Fonction<Grille, Iterable<Grille>> {
 
-	public Humain() {
+    public Humain() {
 
-	}
+    }
 
-	/**
-	 * @param grille
-	 * @return
-	 */
-	public Iterable<Grille> jouer(Grille grille) {
-		HashSet<Grille> g = new HashSet<>();
-		Scanner sc = new Scanner(System.in);
-		int x = 0, y = 0;
-		do {
-			System.out.println("position largeur x: ");
-			x = sc.nextInt();
-			System.out.println("position hauteur y: ");
-			y = sc.nextInt();
+    @Override
+    public Iterable<Grille> get(Grille z) {
+	Grille grille = new Grille(z);
+	HashSet<Grille> g = new HashSet<>();
+	Scanner sc = new Scanner(System.in);
+	int x = 0, y = 0;
+	do {
+	    System.out.println("position largeur x: ");
+	    x = sc.nextInt();
+	    System.out.println("position hauteur y: ");
+	    y = sc.nextInt();
 
-		} while (grille.get(x, y) != Grille.VIDE);
-		grille.placer(x, y);
-		grille.RempliCarres();
-		g.add(grille);
-		return g;
-	}
-
-	@Override
-	public Iterable<Grille> get(Grille x) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	} while (grille.get(x, y) != Grille.VIDE);
+	grille.placer(x, y);
+	grille.RempliCarres();
+	g.add(grille);
+	return g;
+    }
 }

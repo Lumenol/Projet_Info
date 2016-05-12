0c1061e1ef0761ccf281e10ca82fdebe2347b4d5
Bovie Pierre-Edouard <pe.xboxlive@hotmail.fr>
2016-05-12 17:00:52 +0200

    ++

--- src/betaplusplus/Pipopipette.java
+++ src/betaplusplus/Pipopipette.java
@@ -14,7 +14,7 @@ public class Pipopipette {
 					+ "java -jar pipopipette.jar -name affiche vos noms et prénoms\n"
 					+ "java -jar pipopipette.jar -h rappelle la liste des options du programme\n"
 					+ "java -jar pipopipette.jar –graphe type nblignes nbcolonnes renvoie le graphe des configurations réduit au format dot.\n"
-					+ "java -jar pipopipette.jar –joue strategie offre la possibilité de jouer sur la console de manière interactive.\n"
+					+ "java -jar pipopipette.jar –joue joueur strategie offre la possibilité de jouer sur la console de manière interactive. Le paramètre joueur vaut soit 1 ou 2 : 1 indique que l’humain commence, 2 que c’est l’ordinateur. Les valeurs possibles de strategie sont donnés dans la partie « Stratégies élémentaires »\n"
 					+ "java -jar pipopipette.jar -cal strategie calcule une stratégie optimum face à un joueur suivant la stratégie passée en paramètre. La commande renvoie le résultat au format pip (voir l’exemple simple C3x3.pip)\n"
 					+ "java -jar pipopipette.jar –cal -graphe strategie calcule une stratégie optimum face un joueur suivant la stratégie passée en paramètre. La commande renvoie le résultat le graphe réduit au format dot et la pondération des configurations définissant la stratégie calculée.\n"
 					+ "java -jar pipopipette.jar -apprend parametres strategie calcule une stratégie par une méthode d’apprentissage (détail à venir).\n"
@@ -25,10 +25,10 @@ public class Pipopipette {
 			Grille g = new Grille(Integer.parseInt(args[2]), Integer.parseInt(args[3]), Boolean.parseBoolean(args[1]));
 			break;// a finir :S
 		case "-joue":
-			switch (args[1]) {
-			case "-simplet": jeu(new Simplet()); break;
-			case "-prevoyant": jeu(new Prevoyant()); break;
-			case "-idiot": jeu(new Idiot()); break;
+			switch (args[2]) {
+			case "-simplet": jeu(new Simplet(),args[1]); break;
+			case "-prevoyant": jeu(new Prevoyant(),args[1]); break;
+			case "-idiot": jeu(new Idiot(),args[1]); break;
 			case "-pondere": break;// a finir :S
 			}
 			;
@@ -48,17 +48,22 @@ public class Pipopipette {
 		}
 	}
 
-	public static void jeu(Fonction<Grille, Iterable<Grille>> ia) {
+	public static void jeu(Fonction<Grille, Iterable<Grille>> ia, String joueur) {
 		Scanner sc = new Scanner(System.in);
 		Humain a = new Humain();
+		Partie c;
 		System.out.println("Hauteur de grille :");
 		int hauteur_de_grille = sc.nextInt();
 		System.out.println("Largeur de grille :");
 		int Largeur_de_grille = sc.nextInt();
 		System.out.println("Voulez vous des contours sur votre grille? : (true/false)");
 		String contours = sc.next();
-		Partie c = new Partie(hauteur_de_grille, Largeur_de_grille, Boolean.parseBoolean(contours), a, ia);
-		c.nouvellePartie(true);
+		if (Integer.parseInt(joueur) == 1){
+			c = new Partie(hauteur_de_grille, Largeur_de_grille, Boolean.parseBoolean(contours), a, ia);
+		}else{
+			c = new Partie(hauteur_de_grille, Largeur_de_grille, Boolean.parseBoolean(contours), ia, a);
+		}
+		c.nouvellePartie(true, Integer.parseInt(joueur));
 		sc.close();
 	}
 	public static Grille cal() {

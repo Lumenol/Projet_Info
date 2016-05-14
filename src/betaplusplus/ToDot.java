29d074d0a25edd3e2c8384d2fd1fcf78b0b06252
Lumen <credeville.louis.maxime@gmail.com>
2016-05-15 00:33:17 +0200

    debut doc ToDot

--- src/betaplusplus/ToDot.java
+++ src/betaplusplus/ToDot.java
@@ -4,83 +4,89 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 /**
- * 
+ *
  *
  * @param <T>
  */
 public class ToDot<T extends Etat> implements Fonction<T, String> {
 
-	private Fonction<T, Iterable<T>> succ;
-	private Fonction<T, ? extends Number> points;
-	private Fonction<Etat, Integer> id;
-	private HashSet<Etat> visite;
+    private Fonction<T, Iterable<T>> succ;
+    private Fonction<T, ? extends Number> points;
+    private Fonction<Etat, Integer> id;
+    private HashSet<Etat> visite;
 
-	/**
-	 * @param succ
-	 */
-	public ToDot(Fonction<T, Iterable<T>> succ) {
-		this(succ, null);
-	}
+    /**
+     * @param succ
+     */
+    public ToDot(Fonction<T, Iterable<T>> succ) {
+	this(succ, null);
+    }
 
-	/**
-	 * @param succ
-	 * @param points
-	 */
-	public ToDot(Fonction<T, Iterable<T>> succ, Fonction<T, ? extends Number> points) {
-		super();
-		this.succ = succ;
-		this.points = points;
-		id = new FonctionDynamique<>(new Fonction<Etat, Integer>() {
-			private int id = 0;
+    /**
+     * @param succ
+     * @param points
+     *            Associe une nombre a n etat comme le poids par exemple
+     */
+    public ToDot(Fonction<T, Iterable<T>> succ, Fonction<T, ? extends Number> points) {
+	super();
+	this.succ = succ;
+	this.points = points;
+	id = new FonctionDynamique<>(new Fonction<Etat, Integer>() {
+	    private int id = 0;
 
-			@Override
-			public Integer get(Etat x) {
-				// TODO Auto-generated method stub
-				return id++;
-			}
-		});
+	    @Override
+	    public Integer get(Etat x) {
+		// TODO Auto-generated method stub
+		return id++;
+	    }
+	});
 
-	}
+    }
 
-	/**
-	 * @param x
-	 * @return
-	 */
-	@Override
-	public String get(T x) {
-		visite = new HashSet<>();
-		StringBuffer sb = new StringBuffer("digraph default{");
-		sb.append("graph[labelloc=\"t\" fontsize=16 fontcolor=\"blue\"\n");
-		sb.append("label=\"Graphe\"]\n");
-		sb.append("node [shape=box fontname = \"Courier New\" color=\"sienna\"]\n");
-		sb.append("edge [fontname = \"Times\" fontcolor=\"sienna\"]\n\n");
-		sb.append(dot(x));
-		sb.append("}");
-		return sb.toString();
-	}
+    /**
+     * Retourne tout les successeur d'un etat au format dot
+     * 
+     * @param x
+     * @return
+     */
+    @Override
+    public String get(T x) {
+	visite = new HashSet<>();
+	StringBuffer sb = new StringBuffer("digraph default{");
+	sb.append("graph[labelloc=\"t\" fontsize=16 fontcolor=\"blue\"\n");
+	sb.append("label=\"Graphe\"]\n");
+	sb.append("node [shape=box fontname = \"Courier New\" color=\"sienna\"]\n");
+	sb.append("edge [fontname = \"Times\" fontcolor=\"sienna\"]\n\n");
+	sb.append(dot(x));
+	sb.append("}");
+	return sb.toString();
+    }
 
-	/**
-	 * @param x
-	 * @return
-	 */
-	private String dot(T x) {
-		if (!visite.contains(x)) {
-			StringBuffer sb = new StringBuffer(id.get(x).toString());
-			// inserer representation
-			sb.append(" [ label= \"");
-			if (points != null)
-				sb.append("V=" + points.get(x) + "\\n");
-			sb.append(x.label() + "\"]\n");
-			for (Iterator<T> iterator = succ.get(x).iterator(); iterator.hasNext();) {
-				T fil = iterator.next();
-				sb.append(dot(fil));
-				// inserer ornement arc ici
-				sb.append(id.get(x) + " -> " + id.get(fil) + "\n");
-			}
-			visite.add(x);
-			return sb.toString();
-		} else
-			return "";
-	}
+    /**
+     * Ecrit l'etat au format dot avec les arc vers ses successeur
+     *
+     * @param x
+     *            Etat
+     * @return chaine de Etat au format dot
+     */
+    private String dot(T x) {
+	if (!visite.contains(x)) {
+	    StringBuffer sb = new StringBuffer(id.get(x).toString());
+	    // inserer representation
+	    sb.append(" [ label= \"");
+	    if (points != null)
+		sb.append("V=" + points.get(x) + "\\n");
+	    sb.append(x.label() + "\"]\n");
+	    for (Iterator<T> iterator = succ.get(x).iterator(); iterator.hasNext();) {
+		T fil = iterator.next();
+		sb.append(dot(fil));
+		// inserer ornement arc ici
+		sb.append(id.get(x) + " -> " + id.get(fil) + "\n");
+	    }
+	    visite.add(x);
+	    return sb.toString();
+	} else
+	    return "";
+    }
 
 }
